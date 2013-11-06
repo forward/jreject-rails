dfa1d67795c25dbbfa34f7eda18c38f72a4f979d
Konstantin Kalbazov <kalbazov@gmail.com>
2013-11-07 02:46:12 +0400

    Bugfix for assets precompile

--- lib/jreject_rails/engine.rb
+++ lib/jreject_rails/engine.rb
@@ -1,8 +1,8 @@
 module JRejectRails
   module Rails
     class Engine < ::Rails::Engine
-      initializer :assets do |config|
-        Rails.application.config.assets.precompile += %w(jquery.reject.js jquery.reject.css)
+      initializer :assets do |app|
+        app.config.assets.precompile += %w(jquery.reject.js jquery.reject.css)
       end
     end
   end
